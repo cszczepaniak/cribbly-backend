ee2095bcdf8416db0f2818ab3cdf8433dbf74057
Connor Szczepaniak <cszczepaniak@gmail.com>
2020-12-14 22:44:54 -0600

    rm weather controller and add a simple hello world controller

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CribblyBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            return await Task.Run(() =>
+            {
+                return Ok("Hello, cribbly!");
+            });
+        }
+    }
+}

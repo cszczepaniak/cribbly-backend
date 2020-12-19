09120091b787acd9ad68e59930c11fbae12e4a36
Connor Szczepaniak <cszczepaniak@gmail.com>
2020-12-19 10:41:28 -0600

    add the right things to columns

--- Migrations/20201219_AddPlayerTable.cs
+++ Migrations/20201219_AddPlayerTable.cs
@@ -7,19 +7,19 @@ namespace CribblyBackend.Migrations
     {
         public override void Down()
         {
-            Delete.Table("Teams");
             Delete.Table("Players");
+            Delete.Table("Teams");
         }
 
         public override void Up()
         {
             Create.Table("Teams")
-                .WithColumn("Id").AsInt32().PrimaryKey();
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity();
             Create.Table("Players")
-                .WithColumn("Id").AsInt32().PrimaryKey()
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
                 .WithColumn("Email").AsString(255).NotNullable().Unique()
-                .WithColumn("Name").AsString(255)
-                .WithColumn("Role").AsString(255)
+                .WithColumn("Name").AsString(255).Nullable()
+                .WithColumn("Role").AsString(255).Nullable()
                 .WithColumn("TeamId").AsInt32().ForeignKey("Teams", "Id").Nullable();
         }
     }

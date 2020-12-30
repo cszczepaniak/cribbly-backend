cc321d3fbd3029fcdaf53b7e139ad368e0195d94
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2020-12-30 00:48:01 -0600

    fix merge conflict?

---
+++ CribblyBackend/Controllers/SampleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CribblyBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return await Task.Run(() =>
+            {
+                return Ok("Hello, cribbly!");
+            });
+        }
+        [HttpGet("private")]
+        [Authorize]
+        public async Task<IActionResult> GetPrivate()
+        {
+            return await Task.Run(() =>
+            {
+                return Ok("Hello, private cribbly!");
+            });
+        }
+    }
+}
---
+++ CribblyBackend/Startup.cs
@@ -0,0 +1,80 @@
+using System.Data;
+using System.Reflection;
+using System.Security.Claims;
+using CribblyBackend.Auth;
+using CribblyBackend.Services;
+using FluentMigrator.Runner;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using MySql.Data.MySqlClient;
+
+namespace CribblyBackend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            string domain = $"https://{Configuration["Auth0:Domain"]}/";
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = domain;
+                    options.Audience = Configuration["Auth0:Audience"];
+                    // If the access token does not have a `sub` claim, `User.Identity.Name` will be `null`. Map it to a different claim by setting the NameClaimType below.
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = ClaimTypes.NameIdentifier
+                    };
+                });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("read:sample", policy => policy.Requirements.Add(new HasScopeRequirement("read:sample", domain)));
+            });
+            services.AddFluentMigratorCore()
+                .ConfigureRunner(c => c
+                    .AddMySql5()
+                    .WithGlobalConnectionString(Configuration["MySQL:ConnectionString"])
+                    .ScanIn(Assembly.GetExecutingAssembly()).For.All());
+
+            services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+            services.AddTransient<IDbConnection>(db => new MySqlConnection(Configuration["MySQL:ConnectionString"]));
+            services.AddTransient<IPlayerService, PlayerService>();
+            services.AddTransient<ITeamService, TeamService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMigrationRunner migrationRunner)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            migrationRunner.MigrateUp();
+        }
+    }
+}

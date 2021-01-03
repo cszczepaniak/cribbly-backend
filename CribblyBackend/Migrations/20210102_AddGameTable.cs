93c6d1a9d088b9a92fa7abe40de1c3965d085b29
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2021-01-02 20:52:06 -0600

    finish game table migration

---
+++ CribblyBackend/Migrations/20210102_AddGameTable.cs
@@ -0,0 +1,29 @@
+using FluentMigrator;
+
+namespace CribblyBackend.Migrations
+{
+    [Migration(20210102072700)]
+    public class AddGameTable : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Assignments");
+            Delete.Table("Games");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Games")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("GameRound").AsInt32().NotNullable()
+                .WithColumn("Type").AsString(255).NotNullable()
+                .WithColumn("ScoreDifference").AsInt32().Nullable()
+                .WithColumn("WinnerId").AsInt32().ForeignKey("Teams", "Id").Nullable();
+            Create.Table("Assignments")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("GameId").AsInt32().ForeignKey("Games", "Id").NotNullable()
+                .WithColumn("TeamId").AsInt32().ForeignKey("Teams", "Id").NotNullable();
+        }
+        
+    }
+}

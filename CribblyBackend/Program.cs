b46954158323fcce0ec3cb356beaf0c1cd3d5565
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2021-01-11 17:18:29 -0600

    configure serilog

--- CribblyBackend/Program.cs
+++ CribblyBackend/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace CribblyBackend
 {
@@ -13,6 +9,12 @@ namespace CribblyBackend
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 

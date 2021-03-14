252518cac3a566533fffbbfd14f307711920ab13
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-03-14 16:34:17 -0500

    reset this to public

--- src/CribblyBackend.DataAccess/Models/Division.cs
+++ src/CribblyBackend.DataAccess/Models/Division.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace CribblyBackend.DataAccess.Models
 {
-    internal class Division
+    public class Division
     {
         public string Name { get; set; }
         public List<Team> Teams { get; set; }

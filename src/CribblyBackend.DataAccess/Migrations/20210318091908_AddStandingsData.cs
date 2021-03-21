b07c1552ba266ff2ecc378c0b654644a62e3a79c
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2021-03-21 12:57:36 -0500

    add migration

---
+++ src/CribblyBackend.DataAccess/Migrations/20210318091908_AddStandingsData.cs
@@ -0,0 +1,29 @@
+using FluentMigrator;
+
+namespace CribblyBackend.Migrations
+{
+    [Migration(20210318091908)]
+    public class AddStandingsData : Migration
+    {
+        public override void Down()
+        {
+            Delete.Column("Division").FromTable("Teams");
+            Delete.Column("Wins").FromTable("Teams");
+            Delete.Column("Losses").FromTable("Teams");
+            Delete.Column("TotalScore").FromTable("Teams");
+            Delete.Column("Seed").FromTable("Teams");
+            Delete.Column("InTournament").FromTable("Teams");
+        }
+
+        public override void Up()
+        {
+            Alter.Table("Teams")
+                .AddColumn("Division").AsString()
+                .AddColumn("Wins").AsInt32()
+                .AddColumn("Losses").AsInt32()
+                .AddColumn("TotalScore").AsInt32()
+                .AddColumn("Seed").AsInt32()
+                .AddColumn("InTournament").AsBoolean();
+        }
+    }
+}

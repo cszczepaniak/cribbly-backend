4b969a8509df858a349877c9debe1aaf8fe9d664
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2021-04-26 23:16:11 -0500

    add service

---
+++ src/CribblyBackend/Services/StandingsService.cs
@@ -0,0 +1,47 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using CribblyBackend.DataAccess.Models;
+using CribblyBackend.DataAccess.Repositories;
+
+namespace CribblyBackend.Services
+{
+    public interface IStandingsService
+    {
+        Task<Team> Calculate(Team team);
+    }
+
+    public class StandingsService : IStandingsService
+    {
+        private readonly IGameRepository _gameRepository;
+        public StandingsService(IGameRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+        public async Task<Team> Calculate(Team team)
+        {
+            var allGames = await _gameRepository.GetByTeamId(team.Id);
+            team.PlayInGames = allGames.Where(g => g.GameRound <= Game.Round.Round3).ToList();
+            team.BracketGames = allGames.Where(g => g.GameRound >= Game.Round.TourneyRound1).ToList();
+
+            team.Wins = allGames.Where(g => g.Winner != null && g.Winner.Name == team.Name).ToList().Count;
+            team.Losses = allGames.Where(g => g.Winner != null && g.Winner.Name != team.Name).ToList().Count;
+
+            foreach(Game game in allGames)
+            {
+                if (game.Winner != null && game.Winner.Name == team.Name)
+                {
+                    team.TotalScore += 121;
+                }
+                else if (game.Winner != null)
+                {
+                    team.TotalScore += (121 - game.ScoreDifference);
+                }
+            }
+
+            return team;
+        }
+    }
+
+
+}

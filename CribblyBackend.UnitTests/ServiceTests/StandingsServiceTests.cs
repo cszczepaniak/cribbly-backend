f1ab700ba401c57b160d05a53a33edec0f5a6d68
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2021-06-13 22:27:42 -0500

    remove unused using statements

--- CribblyBackend.UnitTests/ServiceTests/StandingsServiceTests.cs
+++ CribblyBackend.UnitTests/ServiceTests/StandingsServiceTests.cs
@@ -1,18 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Data.Common;
 using System.Threading.Tasks;
-using CribblyBackend.Controllers;
 using CribblyBackend.DataAccess.Models;
 using CribblyBackend.DataAccess.Repositories;
 using CribblyBackend.Services;
-using Dapper;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Moq;
-using Moq.Dapper;
 using Xunit;
 
 namespace CribblyBackend.UnitTests

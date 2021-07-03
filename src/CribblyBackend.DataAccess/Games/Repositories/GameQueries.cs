34d80bdf4c16ebbfa1552018ac12f15feb2b66c2
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-07-02 22:26:37 -0500

    fix GetById

--- src/CribblyBackend.DataAccess/Games/Repositories/GameQueries.cs
+++ src/CribblyBackend.DataAccess/Games/Repositories/GameQueries.cs
@@ -3,11 +3,15 @@ namespace CribblyBackend.DataAccess.Games.Repositories
     public static class GameQueries
     {
         public static string GetById =
-            @"SELECT s.GameId, s.TeamId, s.GameScore 
+            @"SELECT
+                s.TeamId, s.GameScore,
+                g.Id, g.GameRound, g.ScoreDifference, g.WinnerId,
+                t.Id, t.Name
             FROM Scores s 
             INNER JOIN Games g on s.GameId = g.Id 
             INNER JOIN Teams t on s.TeamId = t.Id 
             WHERE GameId = @Id";
+
         public static string GetByTeamId =
             @"SELECT s.GameId, s.TeamId, s.GameScore 
             FROM Scores s 
@@ -17,7 +21,9 @@ namespace CribblyBackend.DataAccess.Games.Repositories
             INNER JOIN Teams t2 on s2.TeamId = t2.Id
             WHERE s.TeamId = @Id
             AND s.TeamId != s2.TeamId";
+
         public static string Create = @"INSERT INTO Games(GameRound) VALUES (@GameRound)";
+
         public static string CreateScoresForTeam =
             @"INSERT INTO Scores(GameId, TeamId, GameScore) VALUES ((SELECT MAX(id) FROM Games), @TeamId, 0)";
 
--- src/CribblyBackend.DataAccess/Games/Repositories/GameRepository.cs
+++ src/CribblyBackend.DataAccess/Games/Repositories/GameRepository.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Transactions;
+using CribblyBackend.Core.Common.Exceptions;
 using CribblyBackend.Core.Games.Models;
 using CribblyBackend.Core.Games.Repositories;
 using CribblyBackend.Core.Teams.Models;
@@ -21,22 +23,51 @@ namespace CribblyBackend.DataAccess.Games.Repositories
         public async Task<Game> GetById(int id)
         {
             var teams = new Dictionary<int, Team>();
-            var game = (await _connection.QueryAsync<Game, Team, Game>(
+            var scores = new Dictionary<int, Score>(2);
+            var game = (await _connection.QueryAsync<Score, Game, Team, Game>(
                 GameQueries.GetById,
-                (g, t) =>
+                (s, g, t) =>
                 {
                     if (!teams.TryGetValue(t.Id, out var _))
                     {
                         teams.Add(t.Id, t);
                     }
+                    scores[s.GameScore] = s;
                     return g;
                 },
-                new { Id = id },
-                splitOn: "TeamId"
-            )).FirstOrDefault();
+                new { Id = id }
+            )).First();
+            if (teams.Count != 2)
+            {
+                throw new UnexpectedGameDataException(game.Id, $"Expected two teams, got {teams.Count}");
+            }
             game.Teams = teams.Values.ToList();
+            if (scores.Count < 2)
+            {
+                if (scores.ContainsKey(0))
+                {
+                    // both scores are zero, nothing else to do
+                    return game;
+                }
+                // why don't we have two scores?
+                throw new UnexpectedGameDataException(game.Id, $"Expected 2 unique scores, got {scores.Count}");
+            }
+            if (scores.Count > 2)
+            {
+                throw new UnexpectedGameDataException(game.Id, $"Expected 2 unique scores, got {scores.Count}");
+            }
+            // great, we have exactly two!
+            if (!scores.ContainsKey(121))
+            {
+                // but why wasn't one of them 121?
+                var scoreStr = string.Join(" and ", scores.Values.Select(v => v.ToString()));
+                throw new UnexpectedGameDataException(game.Id, $"Expected exactly one score to be 121, got {scoreStr}.");
+            }
+            game.ScoreDifference = Math.Abs(121 - scores.Keys.Where(s => s != 121).Single());
+            game.Winner = new() { Id = scores[121].TeamId };
             return game;
         }
+
         public async Task<IEnumerable<Game>> GetByTeamId(int id)
         {
             return (await _connection.QueryAsync<Game, Team, Team, Game>(
@@ -49,6 +80,7 @@ namespace CribblyBackend.DataAccess.Games.Repositories
                 new { Id = id }
             )).ToList();
         }
+
         public async Task Create(Game game)
         {
             using var scope = new TransactionScope();

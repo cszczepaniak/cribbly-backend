c393e06cf16d97d92d32d2cffdadb41536572b62
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-07-05 18:43:27 -0500

    make stuff actually work

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,17 +8,12 @@ function run_tests() {
 	dotnet test CribblyBackend.UnitTests
 }
 
-function docker_login() {
-    aws ecr get-login-password --region "$AWS_REGION"| docker login --username AWS --password-stdin "$ECR_REPO_URI"
-}
-
 function build_project() {
-    docker build -t "cribbly-backend:$BUILD_NAME"
+    docker build -t "$CONTAINER_URI" src/CribblyBackend
 }
 
 function upload_docker_image() {
     if [ $IS_PR = true ]; then # TODO TURN THIS BACK TO FALSE!!!!!
-        docker tag "cribbly-backend:$BUILD_NAME" "$CONTAINER_URI"
         docker push "$CONTAINER_URI"
     else
         echo "PR build; skipping container upload..."
@@ -27,7 +22,6 @@ function upload_docker_image() {
 
 function main() {
     run_tests
-    docker_login
     build_project
     upload_docker_image
 }
--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,5 +1,5 @@
 export BUILD_NAME=${GITHUB_SHA:0:7}
-export ECR_REPO_URI="$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com"
+export ECR_REPO_URI="$ECR_REGISTRY/cribbly-backend"
 export CONTAINER_URI="$ECR_REPO_URI:$BUILD_NAME"
 
 if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then

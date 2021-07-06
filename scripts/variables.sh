567cbdc44238384f085a565d5ba5bfd065bfb581
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-07-05 20:38:12 -0500

    define ecr registry

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,4 +1,5 @@
 export BUILD_NAME=${GITHUB_SHA:0:7}
+export ECR_REGISTRY="$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com"
 export ECR_REPO_URI="$ECR_REGISTRY/cribbly-backend"
 export CONTAINER_URI="$ECR_REPO_URI:$BUILD_NAME"
 export DB_NAME="cribblydb"

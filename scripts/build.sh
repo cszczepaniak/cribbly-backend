752388eb24590cfea9b64db1cf1189ee351094b3
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-07-10 19:51:02 -0500

    rework all the build stuff

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,37 +2,16 @@
 
 set -ex
 
-source ./scripts/variables.sh
-
 function run_tests() {
 	dotnet test CribblyBackend.UnitTests
 }
 
 function build_project() {
-    dotnet publish src/CribblyBackend -c Release -o $BUILD_OUTPUT_DIR
+    dotnet publish src/CribblyBackend -c Release
 }
-
-function publish_container {
-    aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URL
-    docker build -t $ECR_REGISTRY:$BUILD_NAME .
-    docker push $ECR_REGISTRY:$BUILD_NAME
-}
-
-function upload_artifact() {
-    if [ $IS_PR = false ]; then
-        pushd $BUILD_OUTPUT_DIR
-            zip -r $ARTIFACT_PATH *
-        popd
-        aws s3 cp $ARTIFACT_PATH $S3_ARTIFACT_URI
-    else
-        echo "PR build; skipping artifact upload..."
-    fi
-}
-
 function main() {
     run_tests
     build_project
-    # publish_container
 }
 
 main

9bbbe3283c25439c328ebc3d7ac576891cb7f938
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-08-28 16:04:21 -0500

    add persistence options

--- src/CribblyBackend.DataAccess/Extensions/ServiceCollectionExtensions.cs
+++ src/CribblyBackend.DataAccess/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using CribblyBackend.Core.Divisions.Repositories;
 using CribblyBackend.Core.Games.Repositories;
 using CribblyBackend.Core.Players.Repositories;
@@ -9,6 +10,7 @@ using CribblyBackend.DataAccess.Players.Repositories;
 using CribblyBackend.DataAccess.Teams.Repositories;
 using CribblyBackend.DataAccess.Tournaments.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace CribblyBackend.DataAccess
 {
@@ -16,13 +18,32 @@ namespace CribblyBackend.DataAccess
     {
         public static IServiceCollection AddDataAccess(this IServiceCollection services)
         {
-            return services
-                .AddTransient<IPlayerRepository, PlayerRepository>()
-                .AddTransient<ITeamRepository, TeamRepository>()
-                .AddTransient<IGameRepository, GameRepository>()
-                .AddTransient<IDivisionRepository, DivisionRepository>()
-                .AddTransient<ITournamentRepository, TournamentRepository>()
-                .AddTransient<ITournamentPlayerRepository, TournamentPlayerRepository>();
+            var persister = Environment.GetEnvironmentVariable("CRIBBLY_PERSISTER").ToLower();
+            Log.Information("Using {@persister} for persistence", persister);
+            return services.AddSelectedPersister(persister);
+        }
+        private static IServiceCollection AddSelectedPersister(this IServiceCollection services, string persister)
+        {
+            switch (persister)
+            {
+                case Persisters.MySQL:
+                    return services
+                        .AddTransient<IPlayerRepository, PlayerRepository>()
+                        .AddTransient<ITeamRepository, TeamRepository>()
+                        .AddTransient<IGameRepository, GameRepository>()
+                        .AddTransient<IDivisionRepository, DivisionRepository>()
+                        .AddTransient<ITournamentRepository, TournamentRepository>()
+                        .AddTransient<ITournamentPlayerRepository, TournamentPlayerRepository>();
+                case Persisters.S3:
+                    throw new NotImplementedException("S3 persistence is not implemented");
+                case Persisters.Memory:
+                    throw new NotImplementedException("In-memory persistence is not implemented");
+                case "":
+                    var opts = string.Join(", ", Persisters.S3, Persisters.MySQL, Persisters.Memory);
+                    throw new Exception($"No persister specified. Please set environment variable CRIBBLY_PERSISTER to one of: {opts}");
+                default:
+                    throw new ArgumentException($"Unexpected persistence option: {persister}");
+            }
         }
     }
 }
---
+++ src/CribblyBackend.DataAccess/Persisters.cs
@@ -0,0 +1,9 @@
+namespace CribblyBackend.DataAccess
+{
+    public static class Persisters
+    {
+        public const string S3 = "s3";
+        public const string MySQL = "mysql";
+        public const string Memory = "memory";
+    }
+}
